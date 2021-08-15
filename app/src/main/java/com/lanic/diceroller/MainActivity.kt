ee2d66f09c25b49ce207a72b9a33f2f6d0d380fe
takedawon <takedawon@naver.com>
2021-08-15 22:47:00 +0900

    강의 3까지 완료

--- app/src/main/java/com/lanic/diceroller/MainActivity.kt
+++ app/src/main/java/com/lanic/diceroller/MainActivity.kt
@@ -1,22 +1,78 @@
 package com.lanic.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            messageCard("헬로 라닉!")
+            MaterialTheme {
+                messageCard(Message(author = "Android", name = "Jetpack Compose"))
+            }
         }
     }
 
+    data class Message(val author: String, val name: String)
+
     @Composable
-    fun messageCard(name:String) {
-        Text(text = name)
+    fun messageCard(message: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "AppIcon",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = message.name,
+                        modifier = Modifier.padding(4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
+        }
     }
 
+
+    @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        MaterialTheme {
+            messageCard(message = Message("Android", "Test Compose"))
+        }
+    }
 }
