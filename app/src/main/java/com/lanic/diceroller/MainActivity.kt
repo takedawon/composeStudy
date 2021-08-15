79956fceee741ecbe87b7796fdc49e5847ec0c49
takedawon <takedawon@naver.com>
2021-08-15 22:12:02 +0900

    라이브러리 업데이트 및 코드랩 진행

--- app/src/main/java/com/lanic/diceroller/MainActivity.kt
+++ app/src/main/java/com/lanic/diceroller/MainActivity.kt
@@ -10,12 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "라닉")
+            messageCard("헬로 라닉!")
         }
     }
 
     @Composable
-    fun greeting(name:String) {
-        Text(text = "gg")
+    fun messageCard(name:String) {
+        Text(text = name)
     }
+
 }
